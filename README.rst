bd42274d9706ec8f230d1d462f669c3f7a933b90
Stein Magnus Jodal <stein.magnus@jodal.no>
2013-10-05 12:54:18 +0200

    Empty extension created with cookiecutter

---
+++ README.rst
@@ -0,0 +1,27 @@
+Mopidy-Scrobbler
+============================
+
+Mopidy extension for scrobbling played tracks to Last.fm
+
+Usage
+-----
+
+Install by running::
+
+    pip install Mopidy-Scrobbler
+
+Or, if available, install the Debian/Ubuntu package from `apt.mopidy.com
+<http://apt.mopidy.com/>`_.
+
+Before starting Mopidy, you must add configuration for
+Mopidy-Scrobbler to your Mopidy configuration file::
+
+    [scrobbler]
+    # TODO: Add example of extension config
+
+Project resources
+-----------------
+
+- `Source code <https://github.com/jodal/mopidy-scrobbler>`_
+- `Issue tracker <https://github.com/jodal/mopidy-scrobbler/issues>`_
+- `Download development snapshot <https://github.com/jodal/mopidy-scrobbler/tarball/master#egg=Mopidy-Scrobbler-dev>`_
---
+++ setup.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+import re
+from setuptools import setup
+
+
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
+
+
+setup(
+    name='Mopidy-Scrobbler',
+    version=get_version('mopidy_scrobbler/__init__.py'),
+    url='https://github.com/jodal/mopidy-scrobbler',
+    license='Apache License, Version 2.0',
+    author='Stein Magnus Jodal',
+    author_email='stein.magnus@jodal.no',
+    description='Mopidy extension for scrobbling played tracks to Last.fm',
+    long_description=open('README.rst').read(),
+    packages=['mopidy_scrobbler'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Mopidy',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'scrobbler = mopidy_scrobbler:Extension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+)
