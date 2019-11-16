0830f131b50d9679e6b2097febc7913bc09e5132
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-11-16 13:54:39 +0100

    Use pkg_resources to read version

--- mopidy_scrobbler/__init__.py
+++ mopidy_scrobbler/__init__.py
@@ -1,8 +1,10 @@
 import pathlib
 
+import pkg_resources
+
 from mopidy import config, ext
 
-__version__ = "1.2.1"
+__version__ = pkg_resources.get_distribution("Mopidy-Scrobbler").version
 
 
 class Extension(ext.Extension):
